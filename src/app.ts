5c131420184aba6dcca30949ab2ec5ceaa656d4e
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-03-31 15:04:42 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import logger from "./config/logger";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res, next) => {
+  const error = createHttpError(401, "You cannnot access this route");
+  // throw error;
+  next(error);
+  // res.send("Hello World!");
 });
 
+// gloal error handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.status || 500;
+  logger.error(err.message, {
+    statusCode,
+  });
+
+  res.status(statusCode).json({
+    errors: [{ type: err.name, msg: err.message, path: "", location: "" }],
+  });
+});
 export default app;
