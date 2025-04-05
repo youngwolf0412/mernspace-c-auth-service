a92cd24fd79db0e831040541f189dce43eb58284
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-04-05 18:52:02 +0530

    database Type ORM install, connect, Type ORM Config

---
+++ src/config/data-source.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "../entity/User";
+import { Config } from ".";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: Config.DB_HOST,
+  port: Number(Config.DB_PORT),
+  username: Config.DB_USERNAME,
+  password: Config.DB_PASSWORD,
+  database: Config.DB_NAME,
+
+  // Don't set this to true in production!
+  // It will drop all your data every time you run the app
+  // and it will create the tables again
+  // and it will create the tables again
+  // and it will create the tables again
+  synchronize: Config.NODE_ENV === "test" || Config.NODE_ENV === "dev",
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,16 @@
 import { config } from "dotenv";
+import path from "path";
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) }); // Load environment variables from .env.test or .env.dev or .env.prod file
 
-config();
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } =
+  process.env;
 
-const { PORT, NODE_ENV } = process.env;
-
-export const Config = { PORT, NODE_ENV };
+export const Config = {
+  PORT,
+  NODE_ENV,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DB_USERNAME,
+  DB_PASSWORD,
+};
