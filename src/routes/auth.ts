1e374baa9b52c278a6bdc874adb22f6ea17bbf7e
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-04-11 10:10:51 +0530

    credntialService dependecny injection in route auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,12 +4,20 @@ import { AuthController } from "../controller/AuthController";
 import { UserService } from "../services/UserService";
 import registerValidator from "../validators/register-validator";
 import { TokenService } from "../services/TokenService";
+import loginValidator from "../validators/login-validator";
+import { CredentialService } from "../services/CredentialService";
 
 const router = Router();
 // userService is an instance of UserService class
 const userService = new UserService();
 const tokenService = new TokenService();
-const authController = new AuthController(userService, logger, tokenService);
+const credentialService = new CredentialService();
+const authController = new AuthController(
+  userService,
+  logger,
+  tokenService,
+  credentialService,
+);
 
 router.post(
   "/register",
@@ -17,6 +25,14 @@ router.post(
   async (req: Request, res: Response, next: NextFunction) => {
     // authController calls the register method from AuthController
     await authController.register(req, res, next);
+  },
+);
+
+router.post(
+  "/login",
+  loginValidator,
+  async (req: Request, res: Response, next: NextFunction) => {
+    await authController.login(req, res, next);
   },
 );
 
